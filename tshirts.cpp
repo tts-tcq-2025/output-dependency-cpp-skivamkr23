c55783d69c3b3d48027ba13807e7cd077b6ddcf9
Shivam <shivamkr02909@gmail.com>
2025-08-01 09:50:50 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -16,7 +16,11 @@ char size(int cms) {
 void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
     assert(size(37) == 'S');
+    assert(size(38) == 'S');
+    assert(size(38) == 'M');
     assert(size(40) == 'M');
+    assert(size(42) == 'M');
+    assert(size(42) == 'L');
     assert(size(43) == 'L');
     std::cout << "All is well (maybe!)\n";
 }
